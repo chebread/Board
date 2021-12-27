ef80a1a5b207e2d2404a1f9157995453be4173bf
차한음 <ihaneum@chahan-eum-ui-MacBookAir.local>
2021-12-27 16:38:30 +0900

    commit: 1

--- exec.py
+++ exec.py
@@ -11,7 +11,7 @@ def FileIsFile(file):
         return -1
 clock = datetime.now()
 day = clock.day
-file = "_board_{}.txt".format(day) # 날짜도 같이
+file = "_board_{}.md".format(day) # 날짜도 같이
 tf = FileIsFile(file)
 if tf == 1:
     print("-- 안내 --")
@@ -22,7 +22,7 @@ else:
     print("-- 안내판 프로그램 --")
     message = str(input("메시지: "))
     # 문자열 앞에다가 문자(=>)를 같이 입력해주는 기능 추가해요
-    mainMessage = "=> " + message + "<="
+    mainMessage = "=> " + message + " <="
     print(mainMessage)
     with open(file, "wb") as w:
         _log_ = pickle.dump(mainMessage, w) # 파일에 기록한다
